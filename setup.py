168dfaad7af8edf68af54db1a167f19c91187001
John Cleaver <cleaver.john.k@gmail.com>
2015-08-14 22:51:03 -0400

    setup: Added some minimal information.

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 __author__ = 'john'
+
+from setuptools import setup
+
+setup(
+    name="mana_archive",
+    version='0.1',
+    description='A database and API for accessing information about cards from the Magic: The Gathering collectible card game.',
+    url='',
+    author='John Cleaver',
+    author_email='cleaver.john.k@gmail.com',
+    license='BSD 3-Clause'
+)
