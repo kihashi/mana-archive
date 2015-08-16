6ccde8cfd16ce228ca149575dc401e16529d4e07
John Cleaver <cleaver.john.k@gmail.com>
2015-08-15 22:08:47 -0400

    model: Added a model for CardType.

--- mana_archive/card_database/model/magic_card/CardType.py
+++ mana_archive/card_database/model/magic_card/CardType.py
@@ -1,7 +1,17 @@
 """
+A SQLAlchemy class for a Magic card type.
 
 Author:    John Cleaver <cleaver.john.k@gmail.com>
 Copyright: 2015 John Cleaver
 License:   BSD (See LICENSE file)
 """
 
+from sqlalchemy import Column, Unicode
+
+class CardType(Base):
+    __table_name__ = "card_type"
+
+    card_type = Column(Unicode, primary_key=True)
+
+    def __repr__(self):
+        return self.card_type
