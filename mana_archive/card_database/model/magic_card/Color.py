9f64608f4d4c59c2668470d482b2130368fb1109
John Cleaver <cleaver.john.k@gmail.com>
2015-08-15 21:46:03 -0400

    model: Added a base for a color object.

--- mana_archive/card_database/model/magic_card/Color.py
+++ mana_archive/card_database/model/magic_card/Color.py
@@ -1,7 +1,19 @@
 """
+A SQLAlchemy class for Magic card colors.
 
 Author:    John Cleaver <cleaver.john.k@gmail.com>
 Copyright: 2015 John Cleaver
 License:   BSD (See LICENSE file)
 """
+
+from sqlalchemy import Column, Unicode, ForeignKey
+
+class Color(Base):
+    __table_name__ = "color"
+
+    color = Column(Unicode, primary_key=True)
+    abbreviation = Column(Unicode)
+
+    def __repr__(self):
+        return self.abbreviation
 
