ea6b0a02403b8e074a1716529d101d64a2896550
John Cleaver <cleaver.john.k@gmail.com>
2015-08-15 22:59:02 -0400

    model: Added a class for a card subtype.

--- mana_archive/card_database/model/magic_card/Subtype.py
+++ mana_archive/card_database/model/magic_card/Subtype.py
@@ -1,7 +1,17 @@
 """
+A SQLAlchemy class for Magic card subtypes
 
 Author:    John Cleaver <cleaver.john.k@gmail.com>
 Copyright: 2015 John Cleaver
 License:   BSD (See LICENSE file)
 """
 
+from sqlalchemy import Column, Unicode
+
+class Subtype(Base):
+    __table_name__ = "subtype"
+
+    subtype = Column(Unicode)
+
+    def __repr__(self):
+        return self.subtype
