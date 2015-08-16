6d0b372519bb66e3a29911dc876a92e70d7e658d
John Cleaver <cleaver.john.k@gmail.com>
2015-08-15 23:01:30 -0400

    model: Added a class for rarity.

---
+++ mana_archive/card_database/model/magic_card/Rarity.py
@@ -0,0 +1,17 @@
+"""
+A SQLAlchemy Class for Magic card rarity
+
+Author:    John Cleaver <cleaver.john.k@gmail.com>
+Copyright: 2015 John Cleaver
+License:   BSD (See LICENSE file)
+"""
+
+from sqlalchemy import Column, Unicode
+
+class Rarity(Base):
+    __table_name__ = "rarity"
+
+    rarity = Column(Unicode)
+
+    def __repr__(self):
+        return self.rarity
