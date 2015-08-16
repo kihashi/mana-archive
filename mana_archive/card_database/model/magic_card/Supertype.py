1f675a4d38c6ff1a41b700c2263951fe29dff352
John Cleaver <cleaver.john.k@gmail.com>
2015-08-15 21:51:15 -0400

    model: Added a supertype model.

--- mana_archive/card_database/model/magic_card/Supertype.py
+++ mana_archive/card_database/model/magic_card/Supertype.py
@@ -1,7 +1,17 @@
 """
+A SQLAlchemy class for Magic card supertypes.
 
 Author:    John Cleaver <cleaver.john.k@gmail.com>
 Copyright: 2015 John Cleaver
 License:   BSD (See LICENSE file)
 """
 
+from sqlalchemy import Column, Unicode
+
+class Supertype(Base):
+    __table_name__ = "supertype"
+
+    supertype = Column(Unicode, primary_key=True)
+
+    def __repr__(self):
+        return self.supertype
