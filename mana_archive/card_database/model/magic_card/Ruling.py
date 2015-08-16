81f10560c5da350bc328dbffb048b80fb86f6537
John Cleaver <cleaver.john.k@gmail.com>
2015-08-15 21:34:41 -0400

    model: Added a card ruling object.

--- mana_archive/card_database/model/magic_card/Ruling.py
+++ mana_archive/card_database/model/magic_card/Ruling.py
@@ -1,7 +1,19 @@
 """
+A SQLAlchemy class for a Magic card ruling.
 
 Author:    John Cleaver <cleaver.john.k@gmail.com>
 Copyright: 2015 John Cleaver
 License:   BSD (See LICENSE file)
 """
 
+from sqlalchemy import Column, Unicode, ForeignKey, Date, UnicodeText
+
+class Ruling(Base):
+    __table_name__ = "ruling"
+
+    date = Column(Date)
+    text = Column(UnicodeText)
+    card = Column(Unicode, ForeignKey('magic_card.name'))
+
+    def __repr__(self):
+        return "[{DATE}]: {TEXT}".format(DATE=self.date, TEXT=self.text)
