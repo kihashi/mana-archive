092ff96b2328adda93c432a8b6f5ac0c23780a0a
John Cleaver <cleaver.john.k@gmail.com>
2015-08-23 23:23:38 -0400

    model: Updated card release to have 2 price columns.

--- mana_archive/card_database/model/magic_card/CardRelease.py
+++ mana_archive/card_database/model/magic_card/CardRelease.py
@@ -7,7 +7,7 @@ License:   BSD (See LICENSE file)
 """
 
 from sqlalchemy import UnicodeText, Integer, Column, Unicode, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 class CardRelease(Base):
     __tablename__ = "card_release"
@@ -19,7 +19,10 @@ class CardRelease(Base):
     expansion = relationship("expansion")
     rarity_id = Column(Unicode, ForeignKey("rarity.rarity"))
     rarity = relationship("rarity")
-    mtgoprice = relationship("mtgoprice", uselist=False, backref="card_release")
+    mtgoprice_id = Column(Integer, ForeignKey("price.id"))
+    mtgoprice = relationship("Price", uselist=False, backref=backref("card_release", uselist=False))
+    tcgplayerprice_id = Column(Integer, ForeignKey("price.id"))
+    tcgplayerprice = relationship("Price", uselist=False, backref=backref("card_release", uselist=False))
 
     def __repr__(self):
         return self.card.name + " - " + self.expansion
