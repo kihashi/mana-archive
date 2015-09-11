44b00b7f13a7c755ee063e8fa1055fcac74cafbf
John Cleaver <cleaver.john.k@gmail.com>
2015-09-10 21:37:55 -0400

    parsers: Added a docstring with usage notes.

--- mana_archive/card_database/db_init/parsers/cards/mtgjson.py
+++ mana_archive/card_database/db_init/parsers/cards/mtgjson.py
@@ -1,7 +1,17 @@
-"""
+"""MTG JSON Parser
+A parser for MTGJSON extended data JSON files.
+MTGJSON is located at http://mtgjson.com/
 
 Author:    John Cleaver <cleaver.john.k@gmail.com>
 Copyright: 2015 John Cleaver
 License:   BSD (See LICENSE file)
+
+Usage:
+    mtgjson.py -h
+    mtgjson.py [-s | --set] FILE
+
+Options:
+    -h --help      Show this message
+    -s --set       Used to parse a single set file.
 """
 
